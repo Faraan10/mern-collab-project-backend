8c6d44f596a28f6a8b56e0622085c8e6f848b07e
leelahanuman <madasuleelahanuman@gmail.com>
2026-04-09 01:16:38 +0530

    Add new database connection improvements

--- src/config/db.js
+++ src/config/db.js
@@ -1,13 +1,25 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.CONNECTION_STRING);
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.error("MongoDB connection failed:", error.message);
-    process.exit(1); // exiting app early as db connection is failed
-  }
-};
+const connectDB=async()=>{
 
+  try{
+    const connStr=process.env.CONNECTION_STRING;
+
+      if (!connStr) {
+      throw new Error("CONNECTION_STRING is missing");
+    }
+
+     const conn = await mongoose.connect(connStr,{
+      serverSelectionTimeoutMS:5000,
+    });
+    
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  }
+  catch(error){
+    console.log("MongoDB connection error:", error.message);
+
+    process.exit(1);
+  }
+
+}
 export default connectDB;
