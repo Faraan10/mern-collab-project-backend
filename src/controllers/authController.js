6272be9c3342264b89dc7f7d22ebef3733aaa3b1
Muhammed <faraanshaik19@gmail.com>
2026-04-10 00:35:42 +0530

    feat: login auth complete

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
 import asyncHandler from "express-async-handler";
 import bcrypt from "bcrypt";
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 export const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
@@ -10,7 +11,7 @@ export const registerUser = asyncHandler(async (req, res) => {
 
   if (existingUser) {
     res.status(400);
-    throw new Error("User already exists");
+    throw new Error("user already exists");
   }
 
   // hashing password with bcrypt
@@ -24,7 +25,33 @@ export const registerUser = asyncHandler(async (req, res) => {
   });
 
   res.status(201).json({
-    message: "User registered successfully",
+    message: "user registered successfully",
     user, // Note: we removed password already in userModel via toJSON
+  });
+});
+
+export const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  const findUser = await User.findOne({ email });
+
+  if (!findUser) {
+    res.status(400);
+    throw new Error("user does not exist");
+  }
+
+  const comparePassword = await bcrypt.compare(password, findUser.password);
+
+  if (!comparePassword) {
+    res.status(401);
+    throw new Error("invalid login credentials");
+  }
+
+  const token = generateToken(findUser._id);
+
+  res.status(200).json({
+    message: "Login successfull",
+    token,
+    user: findUser, // password is removed in the schema
   });
 });
